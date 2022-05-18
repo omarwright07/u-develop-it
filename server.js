9445dd970ffbb7b94d1f540af7021d563be2f999
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-05-18 16:15:44 -0500

    connected the database to the server

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connnect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Starfrog@29',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {

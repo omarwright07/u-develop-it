8eab5c6d2d012456aae3378b1f6bcad566a311de
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-05-19 05:51:17 -0500

    finish the requests for votes

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+        console.log(`Server running on port ${PORT}`);
     });
-  });
+});
